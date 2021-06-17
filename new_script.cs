c0b78d0df37c878d8ab0338ecdc7e697f5c01229
AntonC9018 <anton.curm@gmail.com>
2021-06-17 23:10:01 +0300

    first commit

---
+++ new_script.cs
@@ -0,0 +1,30 @@
+using Godot;
+using System;
+
+namespace Test
+{
+
+    public class TestClass
+    {
+        public static int TestVar;
+    }
+
+    public class new_script : Node
+    {
+        // Declare member variables here. Examples:
+        // private int a = 2;
+        // private string b = "text";
+
+        // Called when the node enters the scene tree for the first time.
+        public override void _Ready()
+        {
+            
+        }
+
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    //  public override void _Process(float delta)
+    //  {
+    //      
+    //  }
+    }
+}
